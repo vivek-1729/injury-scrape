17fa7b93ed982a79416a04387106856838262310
Vivek Shah <Vivek@dhcp-10-250-203-223.harvard.edu>
2023-09-27 16:16:23 -0400

    added links

--- scrape.py
+++ scrape.py
@@ -3,10 +3,13 @@ import pandas as pd
 from os.path import isfile
 
 # URL of the website
-urls = ["https://hashtagbasketball.com/injury/nba-health-and-safety-protocols", "https://hashtagbasketball.com/injury/illness", "https://hashtagbasketball.com/injury/sprained-left-ankle", "https://hashtagbasketball.com/injury/sprained-right-ankle", "https://hashtagbasketball.com/injury/sore-left-knee", "https://hashtagbasketball.com/injury/sore-right-knee", "https://hashtagbasketball.com/injury/concussion", "https://hashtagbasketball.com/injury/sore-left-ankle", "https://hashtagbasketball.com/injury/sore-lower-back", "https://hashtagbasketball.com/injury/left-knee-injury"]
+urls1 = ["https://hashtagbasketball.com/injury/nba-health-and-safety-protocols", "https://hashtagbasketball.com/injury/illness", "https://hashtagbasketball.com/injury/sprained-left-ankle", "https://hashtagbasketball.com/injury/sprained-right-ankle", "https://hashtagbasketball.com/injury/sore-left-knee", "https://hashtagbasketball.com/injury/sore-right-knee", "https://hashtagbasketball.com/injury/concussion", "https://hashtagbasketball.com/injury/sore-left-ankle", "https://hashtagbasketball.com/injury/sore-lower-back", "https://hashtagbasketball.com/injury/left-knee-injury"]
+urls = urls1 + ["https://hashtagbasketball.com/injury/left-knee-injury", "https://hashtagbasketball.com/injury/sore-left-knee", "https://hashtagbasketball.com/injury/sore-left-ankle", "https://hashtagbasketball.com/injury/sore-right-ankle", "https://hashtagbasketball.com/injury/sprained-left-ankle", "https://hashtagbasketball.com/injury/sprained-right-ankle", "https://hashtagbasketball.com/injury/back-injury", "https://hashtagbasketball.com/injury/sore-back", "https://hashtagbasketball.com/injury/sore-lower-back", "https://hashtagbasketball.com/injury/left-hamstring-injury", "https://hashtagbasketball.com/injury/right-hamstring-injury", "https://hashtagbasketball.com/injury/strained-left-hamstring", "https://hashtagbasketball.com/injury/strained-right-hamstring", "https://hashtagbasketball.com/injury/fractured-nose", "https://hashtagbasketball.com/injury/sore-left-foot", "https://hashtagbasketball.com/injury/sore-right-foot", "https://hashtagbasketball.com/injury/fractured-left-hand", "https://hashtagbasketball.com/injury/fractured-right-hand", "https://hashtagbasketball.com/injury/right-hand-injury", "https://hashtagbasketball.com/injury/right-knee-injury", "https://hashtagbasketball.com/injury/bruised-right-knee", "https://hashtagbasketball.com/injury/bruised-right-quadriceps", "https://hashtagbasketball.com/injury/neck-spasms", "https://hashtagbasketball.com/injury/sore-neck"]
+urls = list(set(urls))
 #Already scraped the urls above. TODO: Need to input a new list of urls and run the program
 
 def scrape_data(url):
+    print("Running...")
     if(isfile("data.csv")): #Check if the data.csv already exists
         old_df = pd.read_csv("data.csv") #if it exists, we will append onto it
     else:
